fd266ea0e93dabbc6ac525b3a09f8c76be78b2cd
Sergey LD <novatones1@gmail.com>
2023-09-02 22:52:17 +0500

    docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"main","l":"Main"},{"p":"over","l":"OverClass"},{"p":"over","l":"View"}];updateSearchResults();
